a1d3973fa24c3abe5a3cdcd088b4bcd4a5af82a7
lukasz <lukasz.rola@wp.pl>
2020-09-05 17:48:28 +0200

    MP-51 Implement ChoiceQuestions service

--- lib/models/questions/choice_question.dart
+++ lib/models/questions/choice_question.dart
@@ -1,7 +1,14 @@
-class ChoiceQuestion {
+import 'package:smarttomato/models/questions/question.dart';
+
+class ChoiceQuestion extends Question {
   final String question;
   final String correctAnswer;
   final String variants;
 
-  ChoiceQuestion(this.question, this.correctAnswer, this.variants);
+  ChoiceQuestion({this.question, this.correctAnswer, this.variants});
+
+  @override
+  bool checkIfAnswerIsCorrect(String answer) {
+    throw correctAnswer == answer;
+  }
 }
